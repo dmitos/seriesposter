7e6191a7c49a3ad988e2f20ecee5d8ab5e8f0c3d
dmitos <dmitos@users.noreply.github.com>
2016-03-27 15:19:08 -0300

    Added files via upload

---
+++ seriesposter.php
@@ -0,0 +1,75 @@
+<?php
+echo "||||||  ||||||  ||||||  ||||  ||||||  ||||||\n";
+echo "||      ||   |  ||  ||   ||   ||   |  ||\n";
+echo "||||||  ||||    ||||||   ||   ||||    ||||||\n";
+echo "    ||  ||   |  || ||    ||   ||   |      ||\n";
+echo "||||||  ||||||  ||  ||  ||||  ||||||  ||||||\n";
+echo "||||||  ||||||  |||||| |||||| ||||||  ||||||\n";
+echo "||  ||  ||  ||  ||       ||   ||   |  ||  ||\n";
+echo "||||||  ||  ||  ||||||   ||   ||||    ||||||\n";
+echo "||      ||  ||      ||   ||   ||   |  || ||\n";
+echo "||      ||||||  ||||||   ||   ||||||  ||  ||v1\n";
+
+//pido que ingrese el directorio base
+//por ejemplo /home/nombre/Videos/series tv #en linux
+echo "Ingresar direcotrio raiz donde se encuentran las SERIES: ";
+$directorio1 = trim(fgets(STDIN));
+
+// obtengo el array con los directorios y sus nombres
+$arboldir  = scandir($directorio1);
+// elimino el primero y segundo array, por ser /. /..
+$i = 0;
+	while ($i < 2){
+		unset($arboldir[$i]);
+	$i++;
+	}
+//ordeno el array directorio
+$arraydir = array_values($arboldir);
+$cantidad = count($arraydir);
+echo "se encontraron ".$cantidad." directorios\n";
+//------------------------------------------------------
+
+foreach($arraydir as $serie){
+//echo "serie: ";
+//$serie = trim(fgets(STDIN));
+
+//sustituyo los espacios en blanco o separadores con +
+$remplazar = array(" ","_","-");
+$buscado = str_replace($remplazar, "+",$serie);
+echo "Descargando caratula de ".$serie."\n";
+//path de donde descargo la busqueda con el dato
+$url = 'https://itunes.apple.com/search?term='.$buscado.'&entity=tvSeason';
+//para decodificar el archivo
+$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, $url); 
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$json = curl_exec($ch); 
+	if ($errno = curl_errno($ch)) {
+	    echo json_encode(array('error' => "error en tomar base de itunes"));
+	    exit;
+	}
+	curl_close($ch);    
+	$obj = json_decode($json,TRUE);
+
+	foreach ($obj as $objeto) {
+    $imagen = $objeto[0]['artworkUrl100'];
+
+}
+$imagengrande = str_replace("100x100bb","300x300",$imagen);
+
+//-----------------------------------------------------------
+
+
+//path de donde esta la imagen a descargar
+//$url = 'http://is5.mzstatic.com/image/thumb/Music6/v4/db/07/8d/db078d84-847e-cb58-c9d5-740bec068ac9/source/250x250.jpg';
+//donde se va a descargar la imagen
+// cover.jpg es el nombre cuando se descarge
+$img = $directorio1."/".$serie."/cover.jpg";
+//la descarga se da aca
+file_put_contents($img, file_get_contents($imagengrande));
+
+
+}
+?>
